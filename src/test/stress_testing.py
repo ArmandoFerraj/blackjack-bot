90a5d90e825ddb3d781a6e2747dcfd76759e7304
Armando Ferraj <aferraj22@gmail.com>
2025-03-09 20:05:09 -0400

    stress testing

--- src/test/stress_testing.py
+++ src/test/stress_testing.py
@@ -1,7 +1,5 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    # Load the trained model (replace trainX with your latest run)
     model = YOLO("best.pt")
-    # Predict on test set and save annotated images
-    model.predict(source="test2.jpg", save=True, save_txt=True, name="stress_test_results")
+    model.predict(source="test4.jpg", save=True, save_txt=True, name="stress_test_results")
